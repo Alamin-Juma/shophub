71135af472ecca195c26c580775a31bcd9b7170a
Alamin Juma Magoti <alamin.magoti@teach2give.com>
2024-11-20 11:15:59 +0300

    routing and products page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../assets/logo.png'
 
 type Props = {};
 
@@ -9,16 +11,25 @@ const Header = (props: Props) => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/shophub">ShopHub</Navbar.Brand>
+          <LinkContainer to='/'>
+          <Navbar.Brand>
+          <img src={logo} alt="ShopHub" />
+          ShopHub 
+          </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <LinkContainer to='/cart'>
+              <Nav.Link>
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              </LinkContainer>
+              <LinkContainer to="/login">
+              <Nav.Link >
                 <FaUser /> Sign In
               </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/styles/bootstrap.custom.css";
 
-createRoot(document.getElementById('root')!).render(
+import "./assets/styles/index.css";
+import App from "./App.tsx";
+import HomeScreen from "./screens/HomeScreen.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<HomeScreen />} />
+      {/* Add more routes here if needed */}
+    </Route>
+  )
+);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
